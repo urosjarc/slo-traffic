0e6aa5752d904d1ef761d5bccf9e84db1859f820
urosjarc <jar.fmf@gmail.com>
2024-02-20 19:28:51 +0100

    Increase timeout.

--- src/main/kotlin/com/urosjarc/slotraffic/SloTrafficUtils.kt
+++ src/main/kotlin/com/urosjarc/slotraffic/SloTrafficUtils.kt
@@ -46,7 +46,7 @@ abstract class SloTrafficUtils(
     val client = HttpClient(CIO) {
         install(ContentNegotiation) { json(jsonModule) }
         install(HttpTimeout) {
-            requestTimeoutMillis = 5000
+            requestTimeoutMillis = 10000
         }
     }
 
